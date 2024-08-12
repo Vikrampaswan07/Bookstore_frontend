e0bd477f3aee74c7846b692637d93fb4f38a031f
Vikrampaswan07 <130029534+Vikrampaswan07@users.noreply.github.com>
2024-08-13 01:09:47 +0530

    Frontend commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'background': '#f0f4f8', // Define your custom background color here
+      },
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#570df8",
+          "secondary": "#f000b8",
+          "accent": "#37cdbe",
+          "neutral": "#3d4451",
+          "base-100": "#ffffff",
+          "background": "#f0f4f8", // Your custom background color
+          // You can customize more properties here
+        },
+      },
+    ],
+  },
+}
